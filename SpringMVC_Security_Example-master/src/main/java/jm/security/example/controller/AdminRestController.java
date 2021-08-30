0aeaf5ac122e4eba97ba4f044f943a0341412db9
Андрей <PWNZAsuran@mail.ru>
2021-08-30 12:24:41 +0300

    Initial commit

---
+++ SpringMVC_Security_Example-master/src/main/java/jm/security/example/controller/AdminRestController.java
@@ -0,0 +1,59 @@
+package jm.security.example.controller;
+
+import jm.security.example.model.User;
+import jm.security.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/admin")
+public class AdminRestController {
+    private UserService service;
+
+    @Autowired
+    public void setService(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping(value = "")
+    public ModelAndView adminPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/admin/admin");
+        return modelAndView;
+    }
+
+    @GetMapping("/rest")
+    public List<User> allUsersPage() {
+        return service.allUsers();
+    }
+
+    @PostMapping("/add/rest")
+    public void addUser(@RequestBody User user) {
+        User user1 = new User(user.getUsername(), user.getPassword());
+        Long role = user.getRoles().iterator().next().getId();
+        service.setRole(user1, role);
+        service.addUser(user1);
+    }
+
+    @GetMapping("/edit/rest/{id}")
+    public User getUserByID(@PathVariable Integer id) {
+        return service.getUserById(id);
+    }
+
+    @PostMapping("/edit/rest")
+    public void updateUser(@RequestBody User user) {
+        User user1 = new User(user.getId(), user.getUsername(), user.getPassword());
+        Long role = user.getRoles().iterator().next().getId();
+        service.setRole(user1, role);
+        service.updateUser(user1);
+    }
+
+    @GetMapping("/delete/rest/{id}")
+    public void deleteUserByID(@PathVariable("id") Integer id) {
+        User user = service.getUserById(id);
+        service.deleteUser(user);
+    }
+}
